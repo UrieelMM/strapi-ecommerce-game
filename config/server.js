50d575b91726c6bce3999c7ecef8e88017dadaaa
Urieel Deve <uriel.mejia@corebiz.ag>
2021-12-10 12:03:04 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e142a787d68068b023665e0ec780a662'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7f003acc-454a-4200-86a0-54a592d4ba2f'
+};
